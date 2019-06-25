c484d3129b73e8a6ae62d7cb6923c76f89bf5cf4
Mads H. Danquah <mads@danquah.dk>
2019-06-25 16:40:00 +0200

    admin-php reload-1.2.0 - better handling of fpm output streams

--- images/admin-php/files/bin/fpm-entrypoint.sh
+++ images/admin-php/files/bin/fpm-entrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/sbin/php-fpm7.2 --nodaemonize --fpm-config /etc/php/7.2/fpm/php-fpm.conf
+/usr/sbin/php-fpm7.2 --nodaemonize --fpm-config /etc/php/7.2/fpm/php-fpm.conf | tail -f $LOG_STREAM
