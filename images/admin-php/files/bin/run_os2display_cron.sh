b845127650c25290aa23eb4d1aa119b4ccf69793
Mads H. Danquah <mads@danquah.dk>
2019-06-27 16:39:00 +0200

    Fix environment environment variable naming

--- images/admin-php/files/bin/run_os2display_cron.sh
+++ images/admin-php/files/bin/run_os2display_cron.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ -z "${APP_ENV}:" ]] ; then
+if [[ -z "${SYMFONY_ENV:-}" ]] ; then
   if [[ $# -eq 0 ]] ; then
       echo "Syntax: $0 <environment>"
       exit 1
   else
-    APP_ENV=$1
+    SYMFONY_ENV=$1
   fi
 fi
 
 cd /var/www/admin
-echo "running in $APP_ENV"
+echo "running in $SYMFONY_ENV"
+
+SYMFONY_DEBUG=0
+
+if [[ "${SYMFONY_ENV}" == "dev" ]] ; then
+  SYMFONY_DEBUG=1
+fi;
 
 # The location of console changes between admin-releases - support both.
 if [[ -x app/console ]]; then
@@ -24,4 +30,4 @@ else
 fi
 
 # We pipe stderr to stdout as jobber won't log stderr.
-APP_ENV=${APP_ENV} gosu www-data $CONSOLE os2display:core:cron 2>&1
+SYMFONY_ENV=${SYMFONY_ENV} SYMFONY_DEBUG=${SYMFONY_DEBUG} gosu www-data $CONSOLE os2display:core:cron 2>&1
